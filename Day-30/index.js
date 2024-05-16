f6dc3ff0da1f26b8ed1ae2c4e480cada6a9641e2
Jawaid Ali <jawaidaliupdates@gmail.com>
2024-05-16 14:39:33 +0500

    100-days-of-coding-challenge

---
+++ Day-30/index.js
@@ -0,0 +1,14 @@
+//Question: 88
+function nearestNumber(num) {
+    return Math.round(num);
+}
+;
+console.log(nearestNumber(1.2)); //result: 1
+console.log(nearestNumber(1.6)); //result: 2
+//Question: 89
+function convertNumber(str) {
+    return parseFloat(str);
+}
+;
+console.log(convertNumber("123"));
+console.log(convertNumber("123.123"));
---
+++ Day-30/index.ts
@@ -0,0 +1,29 @@
+//Question: 88
+
+function nearestNumber(num:any){
+    return Math.round(num)
+};
+
+console.log(nearestNumber(1.2));  //result: 1
+console.log(nearestNumber(1.6)); //result: 2
+
+
+//Question: 89
+
+function convertNumber(str:string){
+   return parseFloat(str);
+};
+
+console.log(convertNumber("123"));      // result: 123
+console.log(convertNumber("123.123")); // result: 123.123
+
+
+//Question: 90
+
+function checkNumberOrNot(val:any):boolean {
+    return isNaN(val);
+};
+
+console.log(checkNumberOrNot("number")); // true
+console.log(checkNumberOrNot("number")); // false
+
