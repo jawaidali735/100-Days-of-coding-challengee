5cc6ba123991fc219a6d6443bf89ab55f9f368c0
Jawaid Ali <jawaidaliupdates@gmail.com>
2024-05-17 16:58:16 +0500

    100-days-of-coding-challenge

---
+++ Day-34/index.js
@@ -0,0 +1,17 @@
+//Question 100:
+function findSquareRoot(val) {
+    return Math.sqrt(val);
+}
+;
+console.log(findSquareRoot(144));
+//Question 101:
+function randomIntiger() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+;
+console.log(randomIntiger());
+function findAbspluteDiff(val, val2) {
+    return Math.abs(-val - val2);
+}
+;
+console.log(findAbspluteDiff(5, 5));
---
+++ Day-34/index.ts
@@ -0,0 +1,24 @@
+//Question 100:
+
+function findSquareRoot(val:number):number {
+   return Math.sqrt(val);
+};
+
+console.log(findSquareRoot(144));
+
+//Question 101:
+
+function randomIntiger():number{
+    return Math.floor(Math.random()* 6) + 1
+};
+
+console.log(randomIntiger());
+
+//Question 102:
+
+function findAbspluteDiff(val:number, val2:number):number {
+    return Math.abs(-val - val2)
+ };
+ 
+ console.log(findAbspluteDiff(5,5));
+
