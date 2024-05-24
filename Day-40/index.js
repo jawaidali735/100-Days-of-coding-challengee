76b5541dfa7db81cb86a8fd616d1e1a36adb2781
Jawaid Ali <jawaidaliupdates@gmail.com>
2024-05-24 15:01:54 +0500

    100-days-of-coding-challenge

---
+++ Day-40/index.js
@@ -0,0 +1,19 @@
+//Question 118:
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+;
+//Question 119:
+var number = 0;
+while (number < 5) {
+    console.log("Hello world");
+    number++;
+}
+;
+//Question 120:
+var arr = [1, 2, 3, 4, 5];
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var num = arr_1[_i];
+    console.log(num);
+}
+;
---
+++ Day-40/index.ts
@@ -0,0 +1,23 @@
+//Question 118:
+
+for(let i = 1; i<=10; i++){
+    console.log(i)
+};
+
+
+//Question 119:
+
+let number:number = 0;
+
+while(number < 5){
+    console.log("Hello world");
+    number++;
+};
+
+//Question 120:
+
+let arr:number[] = [1,2,3,4,5]; 
+
+for(let num of arr){
+    console.log(num);
+};
