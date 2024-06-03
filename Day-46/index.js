d6d62a054ab5ddc147e115cce0b955ceb8ab86cb
Jawaid Ali <jawaidaliupdates@gmail.com>
2024-06-03 17:53:55 +0500

    100-days-of-coding-challenge

---
+++ Day-46/index.js
@@ -0,0 +1,22 @@
+//Question 136:
+var arr = [1, 2, 3, 4, 5];
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    console.log("After adding: ".concat(arr[i], ", the sum is: ").concat(sum));
+}
+;
+console.log("Final sum is: ", sum);
+//Question 137:
+console.log("javed");
+console.log("javed");
+console.log("javed");
+console.log("javed");
+try {
+    consol.log("javed");
+}
+catch (err) {
+}
+console.log("javed");
+console.log("javed");
+console.log("javed");
---
+++ Day-46/index.ts
@@ -0,0 +1,35 @@
+//Question 136:
+var arr = [1, 2, 3, 4, 5];
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    
+    console.log(`After adding: ${arr[i]}, the sum is: ${sum}`);
+}
+;
+console.log("Final sum is: ", sum );
+
+//Question 137:
+
+console.log("javed");
+console.log("javed");
+console.log("javed");
+console.log("javed");
+try {
+    consol.log("javed");
+}
+catch (err) {
+}
+console.log("javed");
+console.log("javed");
+console.log("javed");
+
+//Question 138:
+
+// Sample code snippet for setting a breakpoint
+console.log("Before breakpoint");
+// Set a breakpoint on the following line using your browser's developer tools
+console.log(
+  "This line has a breakpoint set on it in the browser's developer tools"
+);
+console.log("After breakpoint");
